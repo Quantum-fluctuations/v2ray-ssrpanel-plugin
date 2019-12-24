097bec34c42fce1f4222ab8b03f4247c7e451229
lovelyou <34223623+lovelyou@users.noreply.github.com>
2019-12-24 20:52:53 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -119,7 +119,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."    
-    DOWNLOAD_LINK="https://github.com/lovelyou/v2ray-ssrpanel-plugin/raw/master/v2ray-linux-64.zip"
+    DOWNLOAD_LINK="https://github.com/lovelyou/v2ray-ssrpanel-plugin/raw/master/v2ray-tw.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
