d8e3883b7f4f8ce780ae4b7c8977a333d63c0094
lovelyou <34223623+lovelyou@users.noreply.github.com>
2019-12-24 17:05:33 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -118,8 +118,8 @@ sysArch(){
 downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
-    colorEcho ${BLUE} "Downloading V2Ray."
-    DOWNLOAD_LINK="https://github.com/ColetteContreras/v2ray-ssrpanel-plugin/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+    colorEcho ${BLUE} "Downloading V2Ray."    
+    DOWNLOAD_LINK="https://github.com/lovelyou/v2ray-ssrpanel-plugin/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
